84040ee38c05d59df11e085b752b7510de3e2d08
alsalivan <alexander.selivanov@tum.de>
2025-05-09 14:35:09 +0200

    up

--- EchoPulse_pytorch/phenaki_trainer_ekg.py
+++ EchoPulse_pytorch/phenaki_trainer_ekg.py
@@ -28,12 +28,12 @@ from einops.layers.torch import Rearrange
 from PIL import Image
 from tqdm.auto import tqdm
 
-from EchoPulse_pytorch.optimizer import get_optimizer
+from EchoPulse_pytorch.optimizer import get_optimizer, LinearWarmup_CosineAnnealing
 from accelerate import Accelerator
 
 # from phenaki_pytorch.phenaki_pytorch import Phenaki
 from EchoPulse_pytorch.phenaki_pytorch_ekg import Phenaki
-from EchoPulse_pytorch.data import ImageDataset, VideoDataset, video_tensor_to_gif, DataLoader
+from EchoPulse_pytorch.data import MultiModalDatasetCMRECG, DataLoader, video_tensor_to_gif, video_tensor_to_pil_first_image
 
 import os
 import sys
@@ -46,9 +46,9 @@ from EchoPulse_pytorch.dataset_private_mp4_preprocess import EchoDataset_from_Vi
 from typing import Union
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.utils.data.distributed import DistributedSampler
-import jax
-import jax.numpy as jnp
+
 import numpy as np
+
 def mask_schedule(ratio, total_unknown, method="cosine"):
     """Generates a mask rate by scheduling mask functions R.
 
@@ -214,6 +214,7 @@ def determine_types(data, config):
 
     return tuple(output)
 
+
 # trainer class
 
 @beartype
@@ -235,10 +236,14 @@ class PhenakiTrainer(object):
         ema_decay = 0.995,
         adam_betas = (0.9, 0.99),
         wd = 0,
+        linear_warmup_start_factor=0.1,
+        linear_warmup_total_iters=100,
+        cosine_annealing_T_max=1000000,
+        cosine_annealing_eta_min=1e-5,
         save_and_sample_every = 1000,
+        logging_steps = 1000,
         num_samples = 25,
         results_folder = './results',
-        amp = False,
         fp16 = False,
         split_batches = True,
         convert_image_to = None,
@@ -246,19 +251,15 @@ class PhenakiTrainer(object):
         sample_texts: Optional[List[str]] = None,
         dataset: Optional[Dataset] = None,
         dataset_fields: Optional[Tuple[str, ...]] = None,
-        losses_file_folder = None,
+        wandb_mode: str = 'disabled',
     ):
-        
-        # rank: int, 
-        # world_size: int):
-        
-        # self.rank = rank
-        # self.world_size = world_size
         
         super().__init__()
         maskgit = phenaki.maskgit
         cvivit = phenaki.cvivit
-        self.losses_file_folder = losses_file_folder
+        self.wandb_mode = wandb_mode
+        
+        self.logging_steps = logging_steps
         assert exists(cvivit), 'cvivit must be present on phenaki'
 
         # define accelerator
@@ -266,10 +267,21 @@ class PhenakiTrainer(object):
         self.accelerator = Accelerator(
             split_batches = split_batches,
             mixed_precision = 'fp16' if fp16 else 'no',
-            device_placement=True
+            device_placement=True,
+            log_with='wandb',
         )
-
-        self.accelerator.native_amp = amp
+        
+        config = {
+            key: value
+            for key, value in locals().items()
+            if key not in {'self', 'config', '__class__', 'cvivit', 'phenaki', 'maskgit'} and not key.startswith('__')
+        }
+        
+        self.accelerator.init_trackers(
+            project_name="CViViT",
+            config=config,
+            init_kwargs={"wandb": {"mode": self.wandb_mode}}
+        )
 
         self.model = phenaki
 
@@ -303,9 +315,10 @@ class PhenakiTrainer(object):
         
         elif exists(self.sample_texts):
             self.sample_texts = sample_texts
+            
         print("---------------------------------------------------------------------------------------------------------------------------------")
         # print(len(self.sample_texts))
-        assert maskgit.unconditional or exists(self.sample_texts), 'if maskgit is to be trained text conditioned, `sample_texts` List[str] or `sample_texts_file_path` must be given'
+        # assert maskgit.unconditional or exists(self.sample_texts), 'if maskgit is to be trained text conditioned, `sample_texts` List[str] or `sample_texts_file_path` must be given'
 
         self.save_and_sample_every = save_and_sample_every
 
@@ -313,11 +326,21 @@ class PhenakiTrainer(object):
         self.sample_num_frames = default(sample_num_frames, num_frames)
         self.train_on_images = train_on_images
 
-        self.ds = EchoDataset_from_Video(folder, self.image_size, num_frames = num_frames, sample_texts=self.sample_texts)
+        self.ds = MultiModalDatasetCMRECG(
+            mode='train',
+            image_size=self.image_size,
+            folder_cmr=f'{folder}/imaging_pretrain/',
+            folder_ecg=f'{folder}/multimodal/ecg_ED_ES/',
+        )
 
         # self.sampler = DistributedSampler(self.ds, num_replicas=self.world_size, rank=self.rank, shuffle=True)
 
-        dl = DataLoader(self.ds, batch_size = batch_size, pin_memory = True, num_workers = cpu_count())
+        dl = DataLoader(
+            self.ds,
+            batch_size = batch_size,
+            pin_memory = True,
+            num_workers = 32
+            )
 
         dl = self.accelerator.prepare(dl)
         self.dl = cycle(dl)
@@ -332,6 +355,14 @@ class PhenakiTrainer(object):
         # optimizer
 
         self.opt = get_optimizer(maskgit.parameters(), lr = train_lr, wd = wd, betas = adam_betas)
+        
+        self.scheduler_optim = LinearWarmup_CosineAnnealing(
+            optimizer=self.opt,
+            linear_warmup_start_factor=linear_warmup_start_factor,
+            linear_warmup_total_iters=linear_warmup_total_iters,
+            cosine_annealing_T_max=cosine_annealing_T_max,
+            cosine_annealing_eta_min=cosine_annealing_eta_min
+        )
 
         # step counter state
 
@@ -424,7 +455,7 @@ class PhenakiTrainer(object):
 
         for _ in range(self.grad_accum_every):
             data = next(self.dl)
-            data = elements_to_device_if_tensor(data, device)
+            # data = elements_to_device_if_tensor(data, device)
             data_kwargs = self.data_tuple_to_kwargs(data)
 
             assert not (self.train_on_images and data_kwargs['videos'].ndim != 4), 'you have it set to train on images, but the dataset is not returning tensors of 4 dimensions (batch, channels, height, width)'
@@ -449,74 +480,51 @@ class PhenakiTrainer(object):
 
         self.opt.step()
         self.opt.zero_grad()
+        
+        self.scheduler_optim.step(self.step)
 
         accelerator.wait_for_everyone()
 
         if self.is_main and self.step % self.save_and_sample_every == 0:
             milestone = self.step // self.save_and_sample_every
             self.save(milestone)
+            self.evaluate_and_visualize(num_eval_samples=self.num_samples)
             
-        #     print ("eval " * 10)
-        #     self.model.eval()
-        #     print ("eval 2" * 10)
-        #     milestone = self.step // self.save_and_sample_every
-
-        #     # whether to pass in texts or not
-
-        #     sample_kwargs = dict()
-
-        #     # if not self.unconditional:
-        #     #     texts = choices(self.sample_texts, k = self.num_samples)
-        #     # else:
-        #     #     texts = (None,) * self.num_samples
-
-        #     # data_kwargs = {'texts': texts, 'ekg' : ekg}
-               
-        #     # method to call
-
-        #     if self.train_on_images:
-        #         sample_method = self.model.sample_images
-        #     else:
-        #         # sample_method = partial(self.model.module.sample, num_frames=self.sample_num_frames)
-        #         sample_method = partial(self.model.sample, num_frames = self.sample_num_frames)
-        #         print ("sample_method, " , sample_method)
-        #     # evaluate in groups, splitting the kwargs appropriately
-
-        #     with torch.no_grad():
-        #         groups = num_to_groups(self.num_samples, self.batch_size)
-        #         args_kwargs_iter = split_args_and_kwargs(batch_size = self.num_samples, split_size = self.batch_size, **data_kwargs)
-
-        #         all_sampled = []
-        #         for group_batch_size, (_, (_, kwargs)) in zip(groups, args_kwargs_iter):
-        #             _kwargs = kwargs if not self.unconditional else dict()
-        #             print ( "data_kwargs ", data_kwargs.keys())
-        #             sampled = sample_method(num_frames = self.sample_num_frames, batch_size = group_batch_size, **_kwargs)
-        #             all_sampled.append(sampled)
-
-        #     # save video and images differently
-
-        #     if not self.train_on_images:
-        #         sampled_videos = torch.cat(all_sampled, dim = 0)
-        #         milestone_folder = self.results_folder / f'videos.{milestone}'
-        #         milestone_folder.mkdir(parents = True, exist_ok = True)
-
-        #         for ind, (video_tensor, video_caption) in enumerate(zip(sampled_videos.unbind(dim = 0), texts)):
-        #             slugged_video_caption = simple_slugify(video_caption) if exists(video_caption) else str(ind)
-        #             video_tensor_to_gif(video_tensor, str(milestone_folder / f'{slugged_video_caption}.gif'))
-        #     else:
-        #         nrows = int(math.sqrt(self.num_samples))
-
-        #         sampled_images = sampled_videos.detach().cpu().float().clamp(0., 1.)
-        #         grid = make_grid(sampled_images, nrow = nrows, normalize = True, value_range = (0, 1))
-
-        #         save_image(grid, str(self.results_folder / f'{milestone}.png'))
-
-            # save checkpoints
-    
-            # self.save(milestone)
-
         self.step += 1
+        
         return total_loss
+    
+    def evaluate_and_visualize(self, num_eval_samples: int = 4):
+        self.model.eval()
+
+        batch = next(self.dl)
+        # batch = elements_to_device_if_tensor(batch, self.device)
+        data_kwargs = self.data_tuple_to_kwargs(batch)
+
+        ekgs = data_kwargs['ekg'][:num_eval_samples]
+
+        with torch.no_grad():
+            generated_videos = self.model.sample(
+                ekg=ekgs,
+                num_frames=self.sample_num_frames,
+                batch_size=ekgs.size(0),
+            )  # [B, C, T, H, W]
+
+        out_folder = self.results_folder / f'eval_step_{self.step}'
+        out_folder.mkdir(parents=True, exist_ok=True)
+
+        for i, video_tensor in enumerate(generated_videos):
+            gif_path = out_folder / f"video_{i}.gif"
+            video_tensor_to_gif(video_tensor.cpu(), gif_path)
+
+            # Log to WandB if enabled
+            if self.accelerator.is_main_process and self.accelerator.trackers:
+                import wandb
+                self.accelerator.log({
+                    f"generated/video_{i}": wandb.Image(video_tensor_to_pil_first_image(video_tensor.cpu()))
+                })
+
+        self.model.train()
 
     def train(
         self,
@@ -527,38 +535,31 @@ class PhenakiTrainer(object):
         with tqdm(
             initial=self.step,
             total=self.train_num_steps,
-            disable=not self.is_main
+            disable=not self.is_main,
+            dynamic_ncols=True,
         ) as pbar:
-            losses = []
-            start_step = self.step
+            
+            step_losses = []
 
             while self.step < self.train_num_steps:
-                # self.model.cond_drop_prob = mask_schedule(float(self.step)/float(self.train_num_steps),0,"cosine")
-                # print(self.model.cond_drop_prob)
+
                 self.model.training_mask_ratio = float(self.step)/float(self.train_num_steps)
+                
                 loss = self.train_step(
                     only_train_generator=only_train_generator,
                     only_train_critic=only_train_critic
                 )
 
-                losses.append(loss)
-                pbar.set_description(f'loss: {loss:.4f}')
+                step_losses.append(loss)
+                pbar.set_postfix(loss=f"{loss:.4f}", refresh=False)
                 pbar.update(1)
 
-                if self.step % 1000 == 0 and self.step != start_step:
-                    filename = f'{self.losses_file_folder}/losses_{start_step}-{self.step}.txt'
-                    with open(filename, 'w') as file:
-                        for l in losses:
-                            file.write(f'{l}\n')
-                    losses = []
-                    start_step = self.step
-
-            # Save any remaining losses after the loop
-            if losses:
-                filename = f'{self.losses_file_folder}/losses_{start_step}-{self.step}.txt'
-                with open(filename, 'w') as file:
-                    for l in losses:
-                        file.write(f'{l}\n')
+                # Log average loss every 1000 steps
+                if self.step % self.logging_steps == 0 and self.step > 0:
+                    if self.accelerator.is_main_process and self.accelerator.trackers:
+                        avg_loss = sum(step_losses) / len(step_losses)
+                        self.accelerator.log({"train/loss": avg_loss})
+                    step_losses = []
 
         self.print('training complete')
 
